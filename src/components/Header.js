4e909c1be11f30923bd6d3c7482413a3d3128734
Paramjit singh <paramjit.offsureit@gmail.com>
2024-05-17 16:47:16 +0530

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Logo from './Logo'
+const Header = () => {
+  return (
+    <header>
+      <div className='h-16'>
+        <div className='container mx-auto'><Logo /></div>
+      </div>
+    </header>
+  )
+}
+
+export default Header
